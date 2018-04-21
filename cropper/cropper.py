1f86bbd7404731396e09fb91ee041b4a397a6d03
gala377 <r.galczak@gmail.com>
2018-04-21 18:38:40 +0200

    ducks cropped properly

--- cropper/cropper.py
+++ cropper/cropper.py
@@ -16,9 +16,9 @@ def read_boxes(file_path: str) -> (str, (int, int), (int, int)):
         for line in file.readlines():
             raw = line.split(" ")
             name = raw[0].replace("-", "")
-            left = tuple(int(p) for p in  raw[1:3])
-            right = tuple(int(p) for p in raw[3:5])
-            yield (name, left, right)
+            origin = tuple(int(p) for p in  raw[1:3])
+            offset = tuple(int(p) for p in raw[3:5])
+            yield (name, origin, offset)
 
 def iter_images(root_dir_path: str) -> (str, np.array, str):
     """
@@ -49,24 +49,14 @@ def convert_path(original_path: str, new_path: str) -> str:
     __LOGGER__.debug(f"original: {original_path} new_path: {new_path} result {path}")
     return '/'.join(path)
 
-def crop_image(image, left, right) -> np.array:
-    new_image = image[left[1]:right[1], left[0]:right[0]]
+def crop_image(image, origin, offset) -> np.array:
+    new_image = image[origin[1]:origin[1]+offset[1], origin[0]:origin[0]+offset[0]]
     return new_image
-
-def adjust_points(p1, p2) -> ((int, int), (int, int)):
-    """
-    Creates 2 new points form p1, p2 so that they meet constraint
-    p1(x1, y1) < p2(x2, y2)
-    """
-    p1_0, p2_0 = (p1[0], p2[0]) if p1[0] < p2[0] else (p2[0], p1[0])
-    p1_1, p2_1 = (p1[1], p2[1]) if p1[1] < p2[1] else (p2[1], p1[1])
-    return (p1_0, p1_1), (p2_0, p2_1) 
 
 """
 Dict of image name and corresponding bounding box
 """
-BOUNDING_BOXES = {name: tuple(adjust_points(left, right)) for name, left, right in read_boxes('bounding_boxes.txt')}
-
+BOUNDING_BOXES = {name: (origin, offset) for name, origin, offset in read_boxes('bounding_boxes.txt')}
 
 def crop_ducks(data_set_path: str, save_path: str):
     """
