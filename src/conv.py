95de0abddd158d34c53ed1290716c9a241554161
lychanl <lycha_@poczta.onet.pl>
2018-05-26 23:06:50 +0200

    fix

--- src/conv.py
+++ src/conv.py
@@ -8,15 +8,15 @@ class Conv(NN):
         NN.__init__(self, 0.01, 0.99, self.TFRECORD_SUFFIX, 50, 50, 3)
 
     def _create_nn(self):
-        current = tf.layers.conv2d(self.X, 3, 16)
+        current = tf.layers.conv2d(self.input, 16, 3, activation=tf.nn.sigmoid)
         current = tf.layers.max_pooling2d(current, 2, 2)
-        current = tf.layers.conv2d(current, 3, 20)
-        current = tf.layers.conv2d(current, 3, 20)
+        current = tf.layers.conv2d(current, 20, 3, activation=tf.nn.sigmoid)
+        current = tf.layers.conv2d(current, 20, 3, activation=tf.nn.sigmoid)
         current = tf.layers.max_pooling2d(current, 2, 2)
-        current = tf.layers.conv2d(current, 5, 24)
-        current = tf.layers.conv2d(current, 4, 28)
-        current = tf.layers.dense(tf.reshape(current, [-1, 448]), 100)
-        current = tf.layers.dense(current, 50)
+        current = tf.layers.conv2d(current, 24, 5, activation=tf.nn.sigmoid)
+        current = tf.layers.conv2d(current, 28, 3, activation=tf.nn.sigmoid)
+        current = tf.layers.dense(tf.reshape(current, [-1, 448]), 100, activation=tf.nn.sigmoid)
+        current = tf.layers.dense(current, 50, activation=tf.nn.softmax)
 
         return current
 
